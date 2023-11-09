9bcdc80772fbe4f9b09059c88e27ba1f6a5919fe
ayneg <ayneg@me.com>
2023-11-09 18:50:16 +0900

    checkboxでタスクを選択できるようにする

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,7 @@
 import { useState, useRef } from 'react'
 import TodoList from './TodoList'
+import {v4 as uuidv4 } from "uuid"
+
 
 export const TodoForm = () => {
     const [todos, setTodos] = useState([
@@ -13,16 +15,23 @@ export const TodoForm = () => {
         //タスクを追加
         const name = todoNameRef.current.value;
         setTodos((prevTodos) => {
-            return [...prevTodos, {id: "1", name: name, completed: false }];
+            return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
         });
         todoNameRef.current.value = null;
     };
+
+    const toggleTodo = (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id ===id);
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    }
 
     return (
         <>
             <input type="text" ref={todoNameRef} />
             <button onClick={handleAddTodo}>Add</button>
-            <TodoList todos={todos}/>
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
         </>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-export const TodoList = ({ todos }) => {
-  return todos.map((todo) => <TodoItem todo={todo} key={todo.name} />);
+export const TodoList = ({ todos, toggleTodo }) => {
+  return todos.map((todo) => <TodoItem todo={todo} key={todo.id} toggleTodo={toggleTodo} />);
 };
 
 export default TodoList 
